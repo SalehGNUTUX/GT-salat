b3e7d7e20413df605a478a1edebdb822e4062245
GNUTUX <salehoukiki@gmail.com>
2025-11-05 21:42:30 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,73 @@
+// اسم التطبيق
+const CACHE_NAME = 'gt-salat-v1';
+
+// الملفات التي سيتم تخزينها في الكاش
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
+];
+
+// تثبيت Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// تفعيل Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// اعتراض الطلبات
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // إرجاع الملف من الكاش إذا كان موجوداً
+        if (response) {
+          return response;
+        }
+
+        // استدعاء الشبكة
+        return fetch(event.request).then(response => {
+          // التحقق من أن الرد صالح
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // نسخ الرد
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      }
+    )
+  );
+});
